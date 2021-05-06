4d9a513d338a088636d29c84e057fdc79e194cd7
Nazar Vynnytskyi <nazar.vynnytski@lohika.com>
2021-05-06 13:30:57 +0300

    init app

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">Hello</header>
+        </div>
+    );
+}
+
+export default App;
